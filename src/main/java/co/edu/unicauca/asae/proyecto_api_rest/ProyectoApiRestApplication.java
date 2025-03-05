fe8d83e938af6c28a39182c66b27362ba4dce58d
JulianPiamba <jufpiamba@unicauca.edu.co>
2025-03-05 18:57:01 -0500

    Agregamos el Proyecto

---
+++ src/main/java/co/edu/unicauca/asae/proyecto_api_rest/ProyectoApiRestApplication.java
@@ -0,0 +1,11 @@
+package co.edu.unicauca.asae.proyecto_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoApiRestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApiRestApplication.class, args);
+	}
+}
